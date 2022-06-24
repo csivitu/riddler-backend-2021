18d489afeb2215b884be1b4f394b4f623b5bc5ff
sanjaybaskaran01 <sanjaybaskaran01@gmail.com>
2022-06-25 00:50:43 +0530

    fix: Leaderboard now displays new users

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const user = require("../models/User");
 const { logger } = require("../logs/logger");
 const { error_codes, success_codes } = require("../tools/error_codes");
+const { objectIdWithTimestamp } = require('../utils/objectId')
 
 router.get("/all", async (req, res, next) => {
   try {
-    const allUsers = await user.find().sort({lastSolve: 1});
+    const allUsers = await user.find({ _id: { $gt: objectIdWithTimestamp('2022/05/24') } }).sort({lastSolve: 1});
     allUsers.sort((a, b) => b.score - a.score);
     const responseJSON = allUsers.map(({ username, score }) => ({
       username,
@@ -16,6 +17,7 @@ router.get("/all", async (req, res, next) => {
     logger.warn(success_codes.S1);
     return res.status(200).json(responseJSON);
   } catch (e) {
+    console.log(e);
     logger.error(error_codes.E0);
     return res.status(500).json({
       error: e,
